f77fbb175cc15ee284876de83ec30c1d62510a2f
savermyas <saver_is_not@bk.ru>
2015-11-26 16:08:10 +0300

    Mendelson workdir extraction

--- Dockerfile
+++ Dockerfile
@@ -6,15 +6,14 @@ RUN yum update; yum install -y java-1.8.0-openjdk unzip tigervnc-server xorg-x11
 RUN curl -L "http://downloads.sourceforge.net/project/mec-as2/install_mendelson_opensource_as2_1.1b47.zip" > mendelson.zip; unzip mendelson.zip -d mendelson; rm -f mendelson.zip
 ADD webpasswd /mendelson/passwd
 ADD vncpasswd /mendelson/vncpasswd
-RUN ls -lah /mendelson
-ADD runas2.sh /mendelson/runas2.sh
+ADD runas2.sh /mendelson/scripts/runas2.sh
 RUN echo "$(cat /mendelson/vncpasswd)" > /tmp/passwd; echo "$(cat /mendelson/vncpasswd)" >> /tmp/passwd; vncpasswd < /tmp/passwd
-#RUN vncserver
+
+RUN mv mendelson /opt/
+
+RUN mkdir /opt/mendelson/wd/jetty9; mv /opt/mendelson/jetty9/etc /opt/mendelson/wd/jetty9/; mv /opt/mendelson/certificates.p12 /opt/mendelson/wd/; mv /opt/mendelson/log4j.properties /opt/mendelson/wd/
+#TODO - edit jetty path in xml
 
 EXPOSE 8080 5901
 
-CMD /mendelson/runas2.sh
-
-#RUN export DISPLAY=:1 && \
-#Xvfb :1 -screen 0 1024x768x16 && \
-#cd ./mendelson && xvfb-run ./mendelson_as2_start.sh
+CMD /opt/mendelson/scripts/runas2.sh
--- runas2.sh
+++ runas2.sh
@@ -1,9 +1,80 @@
 #!/usr/bin/env bash
 # TODO: SIGTERM trap - https://www.ctl.io/developers/blog/post/gracefully-stopping-docker-containers/
+# TODO - clean locks in wd on exit
+   for f in $(find /tmp/ -name ".X*-lock"); do
+      num=$(echo $f | rev | cut -d'X' -f 1 | rev | cut -d- -f1)	#get display number
+      vncserver -kill :$num					#kill the server
+      rm -f $f							#remove the lock
+   done
+
 export DISPLAY=:1
-cd ./mendelson
+cd ./opt/mendelson/wd
 vncserver
 
-sh ./mendelson_as2_start.sh
+
+#=== VNC prepared
+
+
+CLASSPATH=../as2.jar:../jetty9/start.jar
+export CLASSPATH
+
+if [ -d ../jlib ]; then
+    JARDIR=../jlib
+    export JARDIR
+    for jar in `ls $JARDIR/*.jar $JARDIR/*.zip 2>/dev/null`
+    do
+        CLASSPATH=$CLASSPATH:$jar
+    done
+fi
+if [ -d ../jlib/mina ]; then
+    MINADIR=../jlib/mina
+    export MINADIR
+    for jar in `ls $MINADIR/*.jar $MINADIR/*.zip 2>/dev/null`
+    do
+        CLASSPATH=$CLASSPATH:$jar
+    done
+fi
+if [ -d ../jlib/help ]; then
+    HELPDIR=../jlib/help
+    export HELPDIR
+    for jar in `ls $HELPDIR/*.jar $HELPDIR/*.zip 2>/dev/null`
+    do
+        CLASSPATH=$CLASSPATH:$jar
+    done
+fi
+if [ -d ../jlib/vaadin ]; then
+    VAADINDIR=../jlib/vaadin
+    export VAADINDIR
+    for jar in `ls $VAADINDIR/*.jar $VAADINDIR/*.zip 2>/dev/null`
+    do
+        CLASSPATH=$CLASSPATH:$jar
+    done
+fi
+if [ -d ../jetty9/lib ]; then
+    JETTYLIBDIR=../jetty9/lib
+    export JETTYLIBDIR
+    for jar in `ls $JETTYLIBDIR/*.jar $JETTYLIBDIR/*.zip 2>/dev/null`
+    do
+        CLASSPATH=$CLASSPATH:$jar
+    done
+fi
+if [ -d ../jlib/httpclient ]; then
+    HTTPLIBDIR=../jlib/httpclient
+    export HTTPLIBDIR
+    for jar in `ls $HTTPLIBDIR/*.jar $HTTPLIBDIR/*.zip 2>/dev/null`
+    do
+    CLASSPATH=$CLASSPATH:$jar
+    done
+fi
+java -Xmx1024M -Xms92M -classpath $CLASSPATH de.mendelson.comm.as2.AS2 $1 $2 $3 $4 $5 $6 $7 $8 $9
+
+
+# APP started
+
+
+
+
+#sh ./mendelson_as2_start.sh
 
 vncserver -kill :1
+
